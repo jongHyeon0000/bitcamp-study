bc16b460b14faf7d5f5c93ce519e98b2749408d0
jongHyeon0000 <tamxt4047@naver.com>
2022-07-05 17:15:29 +0900

    07.05

---
+++ java_lang/src/Exam07.java
@@ -0,0 +1,8 @@
+public class Exam07 {
+    public static void main(String[] args) {
+        System.out.println('A');
+        System.out.println('가');
+        System.out.println(' ');
+        System.out.println('');
+    }
+}
---
+++ java_lang/src/Hello.java
@@ -0,0 +1,7 @@
+package src;
+
+public class Hello {
+    public static void main(String args[]){
+        
+    }
+}
